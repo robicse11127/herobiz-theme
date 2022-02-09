0591ffb2f061adef6aeb42439b41a6b5d5d30fda
robicse11127 <robizmailbox@gmail.com>
2022-02-09 23:26:53 +0600

    Implementing content-none.php

--- template-parts/page/content-none.php
+++ template-parts/page/content-none.php
@@ -1,1 +1,46 @@
-From content none
+<?php
+/**
+ * Template part for displaying a message that posts cannot be found
+ */
+?>
+
+<section class="no-results not-found">
+	<header class="page-header">
+		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'herobiz' ); ?></h1>
+	</header>
+
+	<div class="page-content">
+		<?php
+		if ( is_home() && current_user_can( 'publish_posts' ) ) :
+
+			printf(
+				'<p>' . wp_kses(
+					/* translators: 1: link to WP admin new post page. */
+					__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'herobiz' ),
+					array(
+						'a' => array(
+							'href' => array(),
+						),
+					)
+				) . '</p>',
+				esc_url( admin_url( 'post-new.php' ) )
+			);
+
+		elseif ( is_search() ) :
+			?>
+
+			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'herobiz' ); ?></p>
+			<?php
+			get_search_form();
+
+		else :
+			?>
+
+			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'herobiz' ); ?></p>
+			<?php
+			get_search_form();
+
+		endif;
+		?>
+	</div>
+</section>
